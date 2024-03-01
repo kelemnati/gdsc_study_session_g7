a4164211e723c1f1ae71306093cacd3abf419938
Nathnael Keleme <124769535+kelemnati@users.noreply.github.com>
2024-03-01 13:00:37 +0300

    Update main.dart

--- Dart-tasks/task-5/flutter_week_5/lib/main.dart
+++ Dart-tasks/task-5/flutter_week_5/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_week_5/home.dart';
+
+import 'home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,11 +13,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
       home: HomePage(),
     );
   }
